21c564a0647ece335b99e00f925f0eaf30b5e5de
danimaetrix <janssenda@gmail.com>
2017-08-23 07:50:44 -0500

    Update

--- programming_exercises_8_22_17/Scanning/Scanning/src/main/java/com/mycompany/scanning/scanning.java
+++ programming_exercises_8_22_17/Scanning/Scanning/src/main/java/com/mycompany/scanning/scanning.java
@@ -15,33 +15,38 @@ public class scanning {
       
  
      double number;
-     String noun1, adjective1, noun2, adjective2, pln1,pln2,pln3,verb,pverb;
+     String noun1, adjective2, noun3, number4, adjective5,pln6,pln7, pln8, verb9, pverb10;
      String passage;
         
      Scanner scantron = new Scanner(System.in);
     
      System.out.println("Please enter a noun:");
-     noun1 = scantron.next(); 
+     noun1 = scantron.nextLine(); 
      System.out.println("Please enter an adjective:");
-     adjective1 = scantron.next();
-     System.out.println("Please enter another noun:");     
-     noun2 = scantron.next();
-     System.out.println("Please enter another adjective:");     
-     adjective2 = scantron.next();  
+     adjective2 = scantron.next();
+     System.out.println("Please enter a noun:");     
+     noun3 = scantron.next();
+     System.out.println("Please enter a number:");     
+     number4 = scantron.next();  
+     System.out.println("Please enter an adjective:");     
+     adjective5 = scantron.next();      
      System.out.println("Please enter a plural noun:");     
-     pln1 = scantron.next();      
+     pln6 = scantron.next();      
      System.out.println("Please enter a plural noun:");     
-     pln2 = scantron.next();      
+     pln7 = scantron.next();
      System.out.println("Please enter a plural noun:");     
-     pln3 = scantron.next();      
+     pln8 = scantron.next();     
      System.out.println("Please enter a verb:");     
-     verb = scantron.next();
+     verb9 = scantron.next();
      System.out.println("Please enter the past form:");     
-     pverb = scantron.next();
+     pverb10 = scantron.next();
 
-     passage = noun1 + ": the " + noun2 + " frontier.\n These are "
-             + "the voyages of the starship" + noun2 +". Its " + adjective2
-             + " year mission: to explore stra nge" + pln1 + pln2;
+     passage = noun1 + ": the " + adjective2 + " frontier.\nThese are "
+             + "the voyages of the starship " + noun3 +".\nIts " + number4
+             + "-year mission: to explore strange\n" + adjective5 + " " + pln6 + ","
+             + " to seek out " + adjective5 + " " + pln7 + " and \n" +
+             adjective5 + " " + pln8 + ", to boldly " + verb9 + " where "
+                     + "no one has " + pverb10 + " before.";
      
         System.out.println(passage);
 
