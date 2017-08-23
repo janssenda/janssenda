95d2469899280282362e807fca946c22baee6264
janssenda <janssenda@gmail.com>
2017-08-23 09:52:43 -0500

    Fizzbuzz

---
+++ fizzbuzz/fizzbuzz/src/main/java/com/mycompany/fizzbuzz/fizzbuzz.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.fizzbuzz;
+
+
+public class fizzbuzz {
+    public static void main(String[] args) {
+        
+            for (int i =0;  i<=100; i++)  {
+                
+                    if (i % 3 == 0 && i% 5 == 0){
+                        System.out.println("FizzBuzz  ---   " + i);
+
+                        
+                    }
+                        
+                    else if (i % 3 == 0){
+                        System.out.println("Fizz  ---   " + i);   
+                      
+                    
+                    
+                    
+                    }
+                        
+                    else if (i % 5 == 0) {
+                        System.out.println("Buzz  ---   " + i);                        
+;               
+                    
+                    
+                    }
+                    
+
+                        
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    }
+                
+            }
+            
+        
+    }
+    
+
+    
+    
+    
+    
+}
