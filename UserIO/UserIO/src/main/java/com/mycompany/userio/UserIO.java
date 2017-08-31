3f62839185fed47a2ee6e5372ece91677c007ead
janssenda <janssenda@gmail.com>
2017-08-31 09:49:33 -0500

    class

---
+++ UserIO/UserIO/src/main/java/com/mycompany/userio/UserIO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.userio;
+
+/**
+ *
+ * @author danimaetrix
+ */
+public interface UserIO {
+    public String readLine();
+    public double readDouble();
+    public int readInt();
+    public boolean readAnswer();
+}
---
+++ UserIO/UserIO/src/main/java/com/mycompany/userio/main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.userio;
+
+/**
+ *
+ * @author danimaetrix
+ */
+public class main {
+    public static void main(String[] args) {
+        
+
+    baseUserIO myUI = new baseUserIO();
+    
+    myUI.readLine();
+    
+    }    
+}
