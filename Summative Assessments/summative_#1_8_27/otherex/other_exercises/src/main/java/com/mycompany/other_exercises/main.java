7b6afd3ad72beaaed62365d0de8fcf9601c55fe5
danimaetrix <janssenda@gmail.com>
2017-09-04 19:59:48 -0500

    asdas

---
+++ Summative Assessments/summative_#1_8_27/otherex/other_exercises/src/main/java/com/mycompany/other_exercises/main.java
@@ -0,0 +1,206 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.other_exercises;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Danimaetrix
+ */
+public class main {
+    public static void main(String[] args) {
+        //dogGenetics();
+        //ArrayMath();
+        //HealthyHearts();
+        numtest();
+    }
+    
+    
+    
+    public static void HealthyHearts(){
+        int age;
+        int [] target = new int [2];
+        Scanner userInput = new Scanner(System.in);
+        
+        System.out.print("What is your age? ");        
+        age = userInput.nextInt();
+        
+        
+        
+        target[0] = (int)Math.round((220-age)*0.5);
+        target[1] = (int)Math.round((220-age)*0.85);
+        
+        System.out.println("Your max heart rate should be: " + (220 - age));
+        System.out.println("Your target HR zone is "+target[0]+" - "+target[1] + " beats per minute");
+        
+        
+        
+    }
+    
+    
+    public static void ArrayMath(){
+        int[][] numstoadd = {{ 1, 90, -33, -55, 67, -16, 28, -55, 15 },
+        { 999, -60, -77, 14, 160, 301 },
+        { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 
+        140, 150, 160, 170, 180, 190, 200, -99 }} ; 
+        
+        for (int i = 0; i < numstoadd.length; i++){
+            System.out.println("#"+(i+1)+" Array Sum: " + addArray(numstoadd[i]));
+        }
+        
+        
+    }    
+    
+    public static int addArray(int[] arr){
+        int sum = 0;
+        
+        for (int i = 0; i < arr.length; i++){
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+    
+    public static void dogGenetics(){
+        String dogname;
+        String[] breedlist = {"St. Bernard", "Chihuahua", 
+            "Dramatic RedNosed Asian Pug", "Common Cure", "King Doberman"};
+        int [] results;   
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("\n\nWhat is your dog's name? ");
+        dogname = userInput.nextLine();
+        System.out.println("Fantastic!  I have just recieved "
+                + "\nthe results from our lab for " + dogname + ".  "
+                + "\nI think you will be very happy!");
+        System.out.println("Here you are: \n");
+        
+        // Get a set of 5 random ints summing to 100
+        results = getRandRange(100,5);
+        
+        // Show results
+        for (int i=0; i<5; i++){            
+            System.out.println(results[i]+"% " + breedlist[i]);            
+        }
+        
+        System.out.println("\nWow! What a wonderful little pupper!!");
+        System.out.println("Have a nice day!!\n\n");
+    }
+    
+    public static int[] getRandRange(int max, int size){
+        /* Returns an integer array of length 'size' containing
+        randomly calculated integers that sum to equal 'max'  */
+        
+        int [] range = new int[size];
+        int randrange = max, randval = 0, psum = 0;
+        
+        for (int i = 0; i<size; i++){
+
+            if (randrange <= 0){
+                randval = 0;
+            }
+            else if (i == 4) {
+                randval = randrange;
+                
+            }
+            else {
+                randval = trueRandom.randInt(1,randrange);
+            }
+     
+            range[i] = randval;          
+            
+            randrange = randrange - randval;
+            psum = psum + randval;
+
+        } 
+        
+        return range;
+    }
+
+
+
+
+    public static void numtest(){
+        String[] breedlist = {"St. Bernard", "Chihuahua", 
+            "Dramatic RedNosed Asian Pug", "Common Cure", "King Doberman"};
+        double [][] results = {{1,0},{2,0},{3,0},{4,0},{5,0}};   
+
+        System.out.println("\n");
+        
+        for (int i = 0; i < 10000; i++){
+        
+            // Get a set of 5 random ints summing to 100
+            getRandRange2d(100,5, results);
+        
+        }
+        
+        
+        
+        
+        
+        // Show results
+        for (int i=0; i<5; i++){            
+            System.out.printf("%2.0f", results[i][0]);
+            System.out.printf(": " + "%5.2f", results[i][1]);                    
+            System.out.println("");
+        }
+        
+        System.out.println("\n");
+    }
+    
+    
+    public static void getRandRange2d(int max, int size, double[][] results){
+        /* Returns an integer array of length 'size' containing
+        randomly calculated integers that sum to equal 'max'  */
+        int randrange = max, randval = 0, psum = 0, j = 0;
+
+        j = trueRandom.randInt(0,4);
+        
+        for (int i = 0; i<size; i++){
+
+            if (randrange <= 0){
+                randval = 0;
+            }
+            else if (i == 4) {
+                randval = randrange;
+                
+            }
+            else {
+                randval = trueRandom.randInt(1,randrange);
+            }
+     
+            results[j][1] = results[j][1] + (double)randval/100; 
+            
+            j = j + 1;
+            
+            if (j == 5){
+                j = 0;
+            }
+            
+            randrange = randrange - randval;
+            psum = psum + randval;
+
+        } 
+        
+    }        
+        
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ Summative Assessments/summative_#2_9_4/DVDCollection/src/main/java/com/dm/dvdcollection/controller/DvdCollectionController.java
@@ -0,0 +1,259 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dm.dvdcollection.controller;
+
+import com.dm.dvdcollection.dao.DvdCollectionDao;
+import com.dm.dvdcollection.dao.fileIOException;
+import com.dm.dvdcollection.dto.Title;
+import com.dm.dvdcollection.ui.DvdCollectionView;
+import java.util.List;
+
+/**
+ *
+ * @author danimaetrix
+ */
+public class DvdCollectionController {
+
+    // Dependency Injection
+    DvdCollectionView view;
+    DvdCollectionDao dao;
+
+    public DvdCollectionController(DvdCollectionDao dao, DvdCollectionView view) throws fileIOException {
+        this.dao = dao;
+        this.view = view;
+    }
+
+    /*
+    // Main program loop
+     */
+    public void run() throws fileIOException {
+        //dao.loadLibrary(dao.loadEncLibFromFile("mytestlib.cplib", null));    // Loads a real data test lib at runtime -- saves test time, not needed.
+        //dao.createLibrary(15);                                            // Used to add a fake library for testing.  Not needed.
+        boolean repeat = true;
+
+        while (repeat) {
+            repeat = evaluateSelection(getSelection());
+        }
+        exitProgram();
+    }
+
+    /*
+    // End main program loop
+     */
+    // Main menu screen
+    private boolean evaluateSelection(int choice) throws fileIOException {
+        boolean repeat = true;
+        switch (choice) {
+            case 1:
+                addTitle();
+                break;
+            case 2:
+                removeTitle();
+                break;
+            case 3:
+                listTitles();
+                view.waitOnUser();
+                break;
+            case 4:
+                editTitle();
+                break;
+            case 5:
+                searchTitles();
+                break;
+            case 6:
+                saveLibrary();
+                break;
+            case 7:
+                loadLibrary();
+                break;
+            case 8:
+                //EXIT
+                repeat = false;
+                break;
+        }
+        return repeat;
+    }
+
+    private int getSelection() {
+        view.showMainMenuBanner();
+        return view.printMainGetChoice();
+    }
+
+    // Lets user find a title to remove
+    private void removeTitle() {
+        removeTitleByEntry();
+    }
+
+    // Removes a title with no user input (for edit menu)
+    private void removeTitle(String key) {
+        removeTitleByKey(key);
+    }
+
+    // User selects a title to remove
+    private void removeTitleByEntry() {
+        boolean confirm;
+        listTitles();
+        do {
+            view.showRemoveTitleBanner();
+            String titlename = view.getDvdTitle();
+
+            try {
+                Title title = dao.searchTitle(titlename);
+                view.showTitleInfo(title);
+                confirm = view.confirmRemoveTitle();
+                if (confirm) {
+                    dao.removeTitle(titlename);
+                    view.removalSuccess();
+                }
+            } catch (Exception e) {
+                view.noEntryException();
+            }
+        } while (view.askRemove());
+    }
+
+    // Title removed automatically from input menu
+    private void removeTitleByKey(String key) {
+        boolean confirm;
+        confirm = view.confirmRemoveTitle();
+        if (confirm) {
+            dao.removeTitle(key);
+            view.removalSuccess();
+        }
+    }
+
+    /* The Title object is passed by reference... so when
+    we modify it in the editor, we must be careful to ensure
+    that the proper information is sent to DAO.  We send it the 
+    old key so the DAO knows what to remove, and we give it the 
+    updated version of the object to put into the map as a replacement
+     */
+    private void editTitle() {
+        int choice;                                         // choice from edit menu (1 - 8)
+
+        do {
+            listTitles();
+
+            view.showEditTitleBanner();                         // edit menu banner
+
+            String titlename = view.getDvdTitle();              // titlename is also the key for the old title object
+            try {                                               // careful to catch exception if search returns null
+                Title title = dao.searchTitle(titlename);       // we retrieve the object here
+                choice = view.showEditMenuGetChoice(title);     // we show the user the object fields to pick from
+                switch (choice) {
+                    case 0:
+                        removeTitle(titlename);                     // perform a quick remove if user picks 0
+                        break;
+                    case 9:
+                        break;
+                    default:
+                        view.showEditorGetEdits(title, choice);     // the user makes changes to 'title'
+                        dao.editTitle(title, titlename);            // the changes are written to the library by DAO
+                        view.editTitleSuccess();
+                        break;
+                }
+            } catch (Exception e) {
+                view.noEntryException();                        // call our handler for null search result
+            }
+        } while (view.askEdit());
+    }
+
+    private void listTitles() {
+        List<Title> titleList = dao.getAllTitles();
+        view.printAllTitles(titleList);
+    }
+
+    private void addTitle() {
+        do {
+            Title title = view.addTitle();
+            dao.addTitle(title.getTitle(), title);
+            view.addTitleSuccess();
+        } while (view.askAdd());
+    }
+
+    private void exitProgram() {
+        view.showExitMessage();
+    }
+
+    // Searches the map for a single title requested by user
+    private void searchTitles() {
+        view.showSearchTitlesBanner();
+        String titlename = view.getDvdTitle();
+        try {
+            Title title = dao.searchTitle(titlename);
+            view.showTitleInfo(title);
+
+        } catch (Exception e) {
+            view.noEntryException();
+        }
+    }
+
+    // Save library to disk using filename and password (if encrypted)
+    private void saveLibrary() throws fileIOException {
+
+        view.showSaveLibraryBanner();
+        switch (view.showSaveMenuGetChoice()) {
+            case 1: {
+                // Non encrypted library save -- password = null
+                try {
+                    dao.writeEncLibToFile(view.getFilename(), null);
+                    view.librarySaveSuccess();
+                    break;
+                } catch (fileIOException e) {
+                    view.showException(e.getMessage());
+                }
+            }
+
+            case 2: {
+                try {
+                    // Encrypted file save.  Filename AND contents are encrypted using AES
+                    dao.writeEncLibToFile(view.getFilename(), view.getUserPassword());
+                    view.librarySaveSuccess();
+                    break;
+                } catch (fileIOException e) {
+                    view.showException(e.getMessage());
+                }
+
+            }
+            case 3: {
+                break;
+            }
+        }
+
+    }
+
+    // Load library from disk using filename and password (if encrypted)
+    private void loadLibrary() throws fileIOException {
+        view.showLoadLibraryBanner();
+        switch (view.showLoadMenuGetChoice()) {
+            case 1: {
+                // No encryption - password = null
+                try {
+                    dao.loadLibrary(dao.loadEncLibFromFile(view.getFilename(), null));
+                    view.libraryLoadSuccess();
+                } catch (fileIOException e) {
+                    view.showException(e.getMessage());
+                }
+                break;
+            }
+
+            case 2: {
+                try {
+                    // Load encryped file.  Password used to unmask filename and read contents
+                    dao.loadLibrary(dao.loadEncLibFromFile(view.getFilename(), view.getUserPassword()));
+                    view.libraryLoadSuccess();
+                } catch (fileIOException e) {
+                    view.showException(e.getMessage());
+                }
+            }
+            break;
+            case 3: {
+                break;
+            }
+        }
+
+    }
+
+}
