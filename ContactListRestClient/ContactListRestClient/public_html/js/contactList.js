9553d1751b1d3b3365613bdc851d0562a96cf7e8
danimaetrix <janssenda@gmail.com>
2017-09-28 22:02:04 -0500

    asd

--- ContactListRestClient/ContactListRestClient/public_html/js/contactList.js
+++ ContactListRestClient/ContactListRestClient/public_html/js/contactList.js
@@ -10,76 +10,104 @@ $(document).ready(function () {
 
 
     loadContacts();
+    $("#edit-button").click(function () {
 
-    $("#add-button").click(function () {
+
+        var haveValidationErrors = checkAndDisplayValidationErrors($("#edit-form").find("input"));
+
+        if (haveValidationErrors) {
+            return false;
+        }
+
 
         $.ajax({
-            type: "POST",
-            url: "http://localhost:8080/contact",
-
+            type: "PUT",
+            url: "http://localhost:8080/contact/" + $("#edit-contact-id").val(),
             data: JSON.stringify({
-
-                firstName: $("#add-first-name").val(),
-                lastName: $("#add-last-name").val(),
-                company: $("#add-company").val(),
-                phone: $("#add-phone").val(),
-                email: $("#add-email").val()
+                contactId: $("#edit-contact-id").val(),
+                firstName: $("#edit-first-name").val(),
+                lastName: $("#edit-last-name").val(),
+                company: $("#edit-company").val(),
+                phone: $("#edit-phone").val(),
+                email: $("#edit-email").val()
 
             }),
-
             headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/json"
             },
-
             "dataType": "json",
 
             success: function () {
                 $("#errorMessages").empty();
-                $("#add-first-name").val("");
-                $("#add-last-name").val("");
-                $("#add-company").val("");
-                $("#add-phone").val("");
-                $("#add-email").val("");
+
+                hideEditForm();
                 loadContacts();
-
-
             },
-
             error: function () {
 
                 $("#errorMessages")
                         .append($("<li>")
                                 .attr({class: "list-group-item list-group-item-danger"})
                                 .text("Error calling web service. Please try again later"));
-
-
-
             }
 
         });
+    });
+});
+$("#add-button").click(function () {
+
+    var haveValidationErrors = checkAndDisplayValidationErrors($("#add-form").find("input"));
+
+    if (haveValidationErrors) {
+        return false;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/contact",
+        data: JSON.stringify({
+
+            firstName: $("#add-first-name").val(),
+            lastName: $("#add-last-name").val(),
+            company: $("#add-company").val(),
+            phone: $("#add-phone").val(),
+            email: $("#add-email").val()
+
+        }),
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        "dataType": "json",
+        success: function () {
+            $("#errorMessages").empty();
+            $("#add-first-name").val("");
+            $("#add-last-name").val("");
+            $("#add-company").val("");
+            $("#add-phone").val("");
+            $("#add-email").val("");
+            loadContacts();
+        },
+        error: function () {
+
+            $("#errorMessages")
+                    .append($("<li>")
+                            .attr({class: "list-group-item list-group-item-danger"})
+                            .text("Error calling web service. Please try again later"));
+        }
 
     });
-
-
-
-
-
 });
-
 
 
 function loadContacts() {
 
     clearContactTable();
-
     var contentRows = $("#contentRows");
-
-
     $.ajax({
         type: "GET",
         url: "http://localhost:8080/contacts",
-
         success: function (contactArray) {
 
 
@@ -87,52 +115,48 @@ function loadContacts() {
                 var name = contact.firstName + " " + contact.lastName;
                 var company = contact.company;
                 var id = contact.contactId;
-
                 var row = "<tr>";
                 row += "<td>" + name + "</td>";
                 row += "<td>" + company + "</td>";
                 row += "<td><a onclick='showEditForm(" + id + ")'>Edit</a></td>";
-                row += "<td><a>Delete</a></td>";
+                row += "<td><a onclick='deleteContact(" + id + ")'>Delete</a></td>";
                 row += "<tr/>";
-
                 contentRows.append(row);
-
-
             });
-
-
-
         },
-
         error: function () {
             $("#errorMessages")
                     .append($("<li>")
                             .attr({class: "list-group-item list-group-item-danger"})
                             .text("Error calling web service. Please try again later"));
+        }
+    });
+}
 
+function deleteContact(id) {
+    $.ajax({
+        type: "DELETE",
+        url: "http://localhost:8080/contact/" + id,
+        success: function () {
+            loadContacts();
         }
     });
 
-
-
 }
+
 
 function clearContactTable() {
 
     $("#contentRows").empty();
-
 }
 
 
 function showEditForm(contactId) {
 
     $("#errorMessages").empty();
-
-
     $.ajax({
         type: "GET",
         url: "http://localhost:8080/contact/" + contactId,
-
         success: function (data, status) {
 
             $("#edit-first-name").val(data.firstName);
@@ -142,37 +166,52 @@ function showEditForm(contactId) {
             $("#edit-email").val(data.email);
             $("#edit-contact-id").val(data.contactId);
         },
-
         error: function () {
             $("#errorMessages")
                     .append($("<li>")
                             .attr({class: "list-group-item list-group-item-danger"})
                             .text("Error calling web service. Please try again later"));
-
         }
     });
-
-
     $("#contactTableDiv").hide();
     $("#editFormDiv").show();
-
 }
 
 function hideEditForm() {
 
     $("#errorMessages").empty();
-
-
     $("#edit-first-name").val("");
     $("#edit-last-name").val("");
     $("#edit-company").val("");
     $("#edit-phone").val("");
     $("#edit-email").val("");
-
-
-
     $("#editFormDiv").hide();
     $("#contactTableDiv").show();
+}
+
+function checkAndDisplayValidationErrors(input) {
+
+    $("#errorMessages").empty();
+
+    var errorMessages = [];
+
+    input.each(function () {
+        if (!this.validity.valid) {
+            var errorField = $("label[for=" + this.id + "]").text();
+            errorMessages.push(errorField + " " + this.validationMessage);
+        }
+    });
+
+    if (errorMessages.length > 0) {
+        $.each(errorMessages, function (index, message) {
+            $("#errorMessages").append($("<li>")
+                    .attr({class: "list-group-item list-group-item-danger"})
+                    .text(message));
+        });
+        return true;
+    } else {
+        return false;
+    }
 
 
 
