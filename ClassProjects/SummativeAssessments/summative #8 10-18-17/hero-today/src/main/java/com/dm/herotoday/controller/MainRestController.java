e8f5066851e756c65a4a5570281f17e297a32c99
janssenda <janssenda@gmail.com>
2017-10-29 00:43:38 -0500

    asdas

--- ClassProjects/SummativeAssessments/summative #8 10-18-17/hero-today/src/main/java/com/dm/herotoday/controller/MainRestController.java
+++ ClassProjects/SummativeAssessments/summative #8 10-18-17/hero-today/src/main/java/com/dm/herotoday/controller/MainRestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @CrossOrigin
@@ -15,13 +16,34 @@ public class MainRestController {
     DataCoordinatorDao dao;
 
     @Inject
-    public MainRestController(DataCoordinatorDao dao){
+    public MainRestController(DataCoordinatorDao dao) {
         this.dao = dao;
     }
 
-    @RequestMapping(value = "/heroes/{id}", method = RequestMethod.GET)
-    public List<Hero> getHero(@PathVariable("id") String id) {
+    @RequestMapping(value = "/heroes/", method = RequestMethod.GET)
+    public List<Hero> getHero(HttpServletRequest req) {
+        String id = null, name = null, type = null, desc = null;
 
-        return dao.getFromHeroes(id);
+        try {
+            id = req.getParameter("id");
+            if (id.equals("null")) id = null;
+        } catch (NullPointerException e) {}
+
+        try {
+            name = req.getParameter("name");
+            if (name.equals("null")) name = null;
+        } catch (NullPointerException e) {}
+
+        try {
+            type = req.getParameter("type");
+            if (type.equals("null")) type = null;
+        } catch (NullPointerException e) {}
+
+        try {
+            desc = req.getParameter("desc");
+            if (desc.equals("null")) desc = null;
+        } catch (NullPointerException e) {}
+
+        return dao.getFromHeroes(id, name);
     }
 }
