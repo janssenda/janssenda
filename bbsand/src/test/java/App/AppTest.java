cc1902967247946aaf7ad2a6f343c8859799664a
janssenda <janssenda@gmail.com>
2018-01-15 02:43:33 -0600

    ASD

---
+++ bbsand/src/test/java/App/AppTest.java
@@ -0,0 +1,25 @@
+package App;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+
+public class AppTest {
+
+    public AppTest() {
+    }
+
+
+    @Test
+    public void first() {
+
+
+    }
+}
+
+
+
+
+
+
